09c40623e46d5ce771d378c5a7244347ed1eb7d9
Ryan Baumann <ryan.baumann@gmail.com>
2016-10-05 10:27:20 -0400

    OSM PBF Parser skeleton

---
+++ parsers/osm_pbf_parser.rb
@@ -0,0 +1,25 @@
+require_relative "../geocollider_parser"
+
+require 'pbf_parser'
+
+class OSMPBFParser
+  extend GeocolliderParser
+
+  def compare(names, places, filenames, csv_writer)
+    filenames.each do |filename|
+      pbf = PbfParser.new(pbf_file)
+      while pbf.next do
+        pbf.ways.each {|way| way(way)}
+      end
+      while pbf.next do
+        pbf.nodes.each {|node| node(node)}
+        pbf.ways.each {|way| way(way)}
+      end
+    end
+  end
+
+  def way(way)
+    way[:tags].keys.select{|t| t =~ /^name(:.+)?$/}.map{|t| way[:tags][t]}.each do |osm_name|
+    end
+  end
+end
