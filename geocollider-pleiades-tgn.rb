89f627fac78341014743a88d5cef616ea68a14d0
Ryan Baumann <ryan.baumann@gmail.com>
2016-10-05 10:31:11 -0400

    Pleiades/TGN driver skeleton

---
+++ geocollider-pleiades-tgn.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+require_relative 'parsers/pleiades_parser'
+require_relative 'parsers/tgn_parser'
+
+pleiades = PleiadesParser.new()
+names, places = pleiades.parse(%w{pleiades-places-latest.csv pleiades-names-latest.csv pleiades-locations-latest.csv})
+$stderr.puts names.first.inspect
+$stderr.puts places.first.inspect
+$stderr.puts names.keys.length
+$stderr.puts places.keys.length
+
+tgn = TGNParser.new()
+CSV.open(ARGV[0], "wb") do |csv|
+  tgn.compare(names, places, ARGV[1..-1], csv)
+end
