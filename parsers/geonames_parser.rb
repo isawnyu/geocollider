f4ac34f26edee7fad0011e6e5310f0e3a0297b18
Ryan Baumann <ryan.baumann@gmail.com>
2016-10-05 16:14:52 -0400

    Remove old GeonamesParser#compare function

--- parsers/geonames_parser.rb
+++ parsers/geonames_parser.rb
@@ -49,37 +49,4 @@ class GeonamesParser
     end
     return lambda_function
   end
-
-  def compare(names, places, filenames, csv_writer)
-    filenames.each do |filename|
-      geonames_data = File.open(filename, "rb")
-      until geonames_data.eof()
-        geonames_csv_string = geonames_data.readline.force_encoding('UTF-8').encode('UTF-8', :invalid => :replace)
-        CSV.parse(geonames_csv_string, :headers => false, :col_sep => "\t", :quote_char => "\u{FFFF}") do |row|
-          geoname = {}
-          geoname["id"] = row[0]
-          geoname["name"] = row[1]
-          geoname["asciiname"] = row[2]
-          geoname["alternatenames"] = row[3].nil? ? [] : row[3].split(',')
-          geoname["latitude"] = row[4].to_f
-          geoname["longitude"] = row[5].to_f
-          geoname["featureclass"] = row[6]
-          geoname["featurecode"] = row[7]
-          geonames_names = ([geoname["name"], geoname["asciiname"]] + geoname["alternatenames"]).uniq.compact
-          geonames_names.each do |name|
-            $stderr.puts "Checking #{name}"
-            if names.has_key?(name)
-              $stderr.puts "Name match, checking places..."
-              names[name].each do |place|
-                $stderr.puts "Checking #{place}"
-                if self.class.check_point(places[place]['point'], Point.new(geoname["latitude"],geoname["longitude"]))
-                  csv_writer << [place, geoname["id"]]
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-  end
 end
