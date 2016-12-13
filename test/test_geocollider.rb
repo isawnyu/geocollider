527f51e87708fb595953326f1371a09b2092b615
Ryan Baumann <ryan.baumann@gmail.com>
2016-12-13 11:01:00 -0500

    Initial Test::Unit stub

---
+++ test/test_geocollider.rb
@@ -0,0 +1,8 @@
+require 'geocollider'
+require 'test/unit'
+
+class TestGeocollider < Test::Unit::TestCase
+  def test_simple
+    assert_equal(1,1)
+  end
+end
---
+++ test/test_pleiades_parser.rb
@@ -0,0 +1,8 @@
+require 'geocollider'
+require 'test/unit'
+
+class TestPleiadesParser < Test::Unit::TestCase
+  def test_simple
+    assert_equal(1,1)
+  end
+end
