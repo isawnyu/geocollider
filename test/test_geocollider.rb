cf0ecfc296d677d564593961438a5319b2e10802
Ryan Baumann <ryan.baumann@gmail.com>
2016-12-13 11:21:42 -0500

    Add haversine distance test

--- test/test_geocollider.rb
+++ test/test_geocollider.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class GeocolliderTest < Minitest::Test
+  def setup
+    @geocollider = Object.new
+    @geocollider.extend(Geocollider::Parser)
+  end
+
   def test_simple
     assert_equal(1,1)
+  end
+
+  def test_haversine_distance
+    assert_equal(0,@geocollider.haversine_distance(0,0,0,0))
+    assert_equal(5837.057346290527,@geocollider.haversine_distance(40.714268,-74.005974,48.856667,2.350987))
   end
 end
