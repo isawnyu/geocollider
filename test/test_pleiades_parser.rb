8d3ffb97bd616b992b4c5c5ec28843e380301efa
Ryan Baumann <ryan.baumann@gmail.com>
2017-01-13 14:20:55 -0500

    Construct PleiadesParser in TestPleiadesParser#setup

--- test/test_pleiades_parser.rb
+++ test/test_pleiades_parser.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class TestPleiadesParser < Minitest::Test
+  def setup
+    @pleiades = Geocollider::PleiadesParser.new()
+  end
+
   def test_simple
     assert_equal(1,1)
   end
